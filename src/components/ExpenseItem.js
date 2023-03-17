af95e8c09d6f811e4e61a12b4eda61b39104e2ff
axhar-ibraheem <azharibraheem482@gmail.com>
2023-03-17 08:37:10 +0530

    props concept

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
   return (
-    <div>
-      <h2>Expense item</h2>
-      <h4>Food Rs 10</h4>
-      <h4>Petrol Rs 100</h4>
-      <h4>Movies Rs 200</h4>
+    <div className="expense-item">
+      <div>{`${props.date.toDateString()} ${props.location}`}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
     </div>
   );
 }
