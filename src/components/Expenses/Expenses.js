9a4bf3d5a26732a3337c5b5290392577e6c22903
axhar-ibraheem <azharibraheem482@gmail.com>
2023-03-22 17:23:35 +0530

    rendering lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,7 +17,12 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
       />
       {props.items.map((item) => (
-        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
       ))}
     </Card>
   );
